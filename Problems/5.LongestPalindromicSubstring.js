73fd5c111d717b9b8bf5b45d8719cdb06bf7a858
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2025-05-23 23:25:00 -0300

    5. Longest Palindromic Substring

--- Problems/5.LongestPalindromicSubstring.js
+++ Problems/5.LongestPalindromicSubstring.js
@@ -1,1 +1,27 @@
-function longestPalindrome(s) {}
+function longestPalindrome(s) {
+    let bestResult = "";
+
+    function palindromSearch(left, right) {
+        while (left >= 0 && right < s.length && s[left] === s[right]) {
+            left--;
+            right++;
+        }
+        return s.substring(left + 1, right);
+    }
+
+    for (let i = 0; i < s.length; i++) {
+        let oddPalindrome = palindromSearch(i, i);
+        let evenPalindrome = palindromSearch(i, i + 1);
+
+        let longerPalindrome =
+            oddPalindrome.length > evenPalindrome.length
+                ? oddPalindrome
+                : evenPalindrome;
+
+        if (longerPalindrome.length > bestResult.length) {
+            bestResult = longerPalindrome;
+        }
+    }
+
+    return bestResult;
+}
