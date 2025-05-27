a760964f7346aa47a8d4459fbbddb00b9a9e2ce3
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2025-05-27 08:14:38 -0300

    6. Zigzag Conversion

--- Problems/6.ZigzagConversion.js
+++ Problems/6.ZigzagConversion.js
@@ -1,18 +1,35 @@
 function convert(s, numRows) {
-    if (numRows === 1 || s.length < numRows) {
-        return s;
-    }
+    if (numRows === 1 || s.length <= numRows) return s;
 
-    const rows = new Array(numRows).fill("");
-    let reverse = false;
+    const result = new Array(numRows).fill("");
+    let direction = 1;
     let count = 0;
 
     for (let i = 0; i < s.length; i++) {
-        rows[count] += s[i];
-        reverse ? count-- : count++;
-        if (count === numRows - 1 || count === 0) {
-            reverse = !reverse;
+        result[count] += s[i];
+        if (count === 0) {
+            direction = 1;
+        } else if (count === numRows - 1) {
+            direction = -1;
         }
+        count += direction;
     }
-    return rows.join("");
+    return result.join("");
 }
+
+// if (numRows === 1 || s.length < numRows) {
+//     return s;
+// }
+
+// const rows = new Array(numRows).fill("");
+// let reverse = false;
+// let count = 0;
+
+// for (let i = 0; i < s.length; i++) {
+//     rows[count] += s[i];
+//     reverse ? count-- : count++;
+//     if (count === numRows - 1 || count === 0) {
+//         reverse = !reverse;
+//     }
+// }
+// return rows.join("");
