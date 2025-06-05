ac2b84991ec8ed92609dc2300645f2ef5ae1a9ef
Gino Ruben Giorgi <ginorubengiorgi@gmail.com>
2025-06-05 06:29:38 -0300

    10. Regular Expression Matching

---
+++ Problems/10.RegularExpressionMatching.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+function isMatch(s, p) {
+    const m = s.length,
+        n = p.length;
+    const dp = new Array(m + 1).fill().map(() => new Array(n + 1).fill(false));
+    dp[0][0] = true; // empty pattern matches empty string
+
+    for (let j = 1; j <= n; j++) {
+        if (p[j - 1] === "*") dp[0][j] = dp[0][j - 2];
+    }
+
+    for (let i = 1; i <= m; i++) {
+        for (let j = 1; j <= n; j++) {
+            if (s[i - 1] === p[j - 1] || p[j - 1] === ".") {
+                dp[i][j] = dp[i - 1][j - 1];
+            } else if (p[j - 1] === "*") {
+                dp[i][j] = dp[i][j - 2]; // zero occurrences
+                if (s[i - 1] === p[j - 2] || p[j - 2] === ".") {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j]; // one or more occurrences
+                }
+            }
+        }
+    }
+    return dp[m][n];
+}
